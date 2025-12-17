74946c2718df485d2bbfadff07f469427fb044a9
Deepramakhal <deepra313131@gmail.com>
2025-12-17 23:15:56 +0530

    V1 Consolidated Updated

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ const App = () => {
             element={<ScoringPage />}
           />
         </Route>
+        <Route path="/consolidated/:ageGroup/:type" element={<ConsolidatedResults />} />
 
         {/* Catch-all */}
         <Route path="*" element={<NotFound />} />
